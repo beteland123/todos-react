f668b86a90ed7d3c7831add68a15a74069670915
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-06-19 21:28:57 +0300

    add to do item

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,7 +1,32 @@
 import InputTodo from './InputTodo'
+import TodosList from './TodosList'
+import { useState } from 'react';
+
 const TodosLogic = () =>{
+  const [todos,setTodos] = useState([
+    {
+      id:1,
+      title: 'Setup development environment',
+      completed : 'true',
+    },
+    {
+      id:2,
+      title:"Develope website and add contnent",
+      completed:false
+    },
+    {
+      id:3,
+      title:'Deploy to live server',
+      completed :false,
+    }
+  ]);
+
   return(
-    <InputTodo/>
+    
+    <div>
+      <InputTodo />
+      <TodosList todosProps={todos} setTodos={setTodos} />
+    </div>
   )
   
 }
