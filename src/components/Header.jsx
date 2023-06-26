bdb370fb32b1cd50d7cc31638bb8d6828e6f781a
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-06-26 18:13:58 +0300

    intilize style objec and import the module style with the classname for the elment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
+import styles from "../styles/Header.module.css";
+
 const Header = ()=> {
+  const headerStyle = {
+    padding: '20px 0',
+    lineHeight: '1.5em',
+    color: '#aeadad',
+    textAlign: 'center',
+  };
   return(
-     <header>
+     <header
+     style={headerStyle} className={styles.header}>
         <h1> Todo</h1>
         <p>itmes will presist in localstrage</p>
     </header>
