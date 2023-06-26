d142f35743ac4492240cce978cd031c1fdd36aad
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-06-26 18:17:38 +0300

    Add the Sass in the package for the todoitem and import appcss to main entry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/App.css';
 
-//
+
+
 import TodoApp from './components/TodoApp';
 
 const domContainer = document.getElementById('root');
